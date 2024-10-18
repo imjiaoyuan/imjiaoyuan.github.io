acf4d3fc77e2e3ba89782e76a018ba2edae436cc
imjiaoyuan <imjiaoyuan@gmail.com>
2024-10-18 18:31:10 +0800

    Update

--- README.md
+++ README.md
@@ -10,6 +10,13 @@
 - 📗 Blogger of the WeChat official account  '猫四叔' .
 - 🎮 Player for RSS / Basketball / [Machines at War 3](https://www.isotope244.com/machines-at-war-3.html) / [VALORANT](https://val.qq.com/main.html) / [Minecraft](https://www.minecraft.net/zh-hans) .
 - 🎨 Minimalist, favoring simple workflows and design, passionate about free software and open-source code .
-- 📫 You can reach me on GitHub at [imjiaoyuan](https://github.com/imjiaoyuan) or via email at imjiaoyuan@gmail.com.
+- 📫 You can reach me on GitHub at [imjiaoyuan](https://github.com/imjiaoyuan) or via email at `imjiaoyuan@gmail.com`.
 
-### [All posts](./toc.md) / [RSS](https://yuanj.top/rss.xml)
+### Posts
+
+- [2024生物学热点](posts/2024-10-09-2024-biology-hotspots.html) / 2024-10-09
+- [C 语言速查手册](posts/2024-10-09-c-manual.html) / 2024-10-09
+- [单细胞转录组图谱揭示拟南芥愈伤组织诱导过程中早期细胞命运转变轨迹](posts/2024-09-07-single-cell-transcriptome-mapping-reveals-early-cell-fate-transition-trajectories-during-callus-induction-in-arabidopsis.html) / 2024-09-07
+- [Bionanoaccess 安装](posts/2024-09-02-bionanoaccess-installation.html) / 2024-09-02
+- [拟南芥根部的单细胞分辨率调控景观](posts/2024-08-30-single-cell-resolution-regulatory-landscape-in-arabidopsis-roots.html) / 2024-08-30
+- [All Posts](./toc.md) / [RSS](https://yuanj.top/rss.xml)
--- update.py
+++ update.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
 
-# 定义一个函数来提取 title 和 date
 def extract_title_and_date(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
@@ -15,7 +14,6 @@ def extract_title_and_date(filename):
         return title, date
     return None, None
 
-# 遍历 posts 目录下的所有 .md 文件
 entries = []
 for filename in os.listdir('posts'):
     if filename.endswith('.md') and filename != 'toc.md':
@@ -23,23 +21,18 @@ for filename in os.listdir('posts'):
         if title and date:
             entries.append((date, title, filename))
 
-# 按照 date 进行降序排序
 entries.sort(key=lambda x: datetime.strptime(x[0], '%Y-%m-%d'), reverse=True)
 
-# 生成 toc.md 文件
+
 with open('toc.md', 'r', encoding='utf-8') as readme_file:
     readme_content = readme_file.read()
 
-# 找到 ### Posts 标题的位置
 posts_index = readme_content.find('### Posts')
 if posts_index != -1:
-    # 保留 ### Posts 标题前的内容
     readme_content = readme_content[:posts_index]
 else:
-    # 如果 ### Posts 标题不存在，则添加一个
     readme_content += '### Posts\n\n'
 
-# 写入目录
 with open('toc.md', 'w', encoding='utf-8') as readme_file:
     readme_file.write(readme_content)
     readme_file.write('### Posts\n\n')
@@ -49,22 +42,17 @@ with open('toc.md', 'w', encoding='utf-8') as readme_file:
         year = date.split('-')[0]
         if year != current_year:
             if current_year is not None:
-                readme_file.write('\n')  # 在年份上方添加一个换行符
+                readme_file.write('\n')
             current_year = year
             readme_file.write(f'**{current_year}**\n\n')
-        # 构建链接
         link = f"- [{title}](posts/{filename.replace('.md', '.html')}) / {date}"
-        # 写入 toc.md 文件
         readme_file.write(link + '\n')
-    readme_file.write('\n')  # 在年份下方添加一个换行符
+    readme_file.write('\n')
 
 print("toc.md has been updated.")
-
-# 生成 RSS 文件
 rss = Element('rss', {'version': '2.0'})
 channel = SubElement(rss, 'channel')
 
-# 添加频道信息
 title = SubElement(channel, 'title')
 title.text = "JiaoYuan's Blog"
 link = SubElement(channel, 'link')
@@ -72,7 +60,6 @@ link.text = 'https://yuanj.top'
 description = SubElement(channel, 'description')
 description.text = '思绪来得快去得也快，偶尔会在这里停留'
 
-# 添加文章条目
 for date, title_text, filename in entries:
     item = SubElement(channel, 'item')
     title = SubElement(item, 'title')
@@ -84,13 +71,32 @@ for date, title_text, filename in entries:
     description = SubElement(item, 'description')
     description.text = f'{title_text} - {date}'
 
-# 将 XML 树转换为字符串并美化输出
 xml_str = tostring(rss, 'utf-8')
 parsed_xml = minidom.parseString(xml_str)
 pretty_xml_str = parsed_xml.toprettyxml(indent="  ")
 
-# 写入 RSS 文件
 with open('rss.xml', 'w', encoding='utf-8') as rss_file:
     rss_file.write(pretty_xml_str)
-
 print("RSS file has been generated.")
+
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    readme_content = readme_file.read()
+
+latest_posts_index = readme_content.find('### Posts')
+if latest_posts_index != -1:
+    readme_content = readme_content[:latest_posts_index]
+else:
+    readme_content += '### Posts\n\n'
+
+with open('README.md', 'w', encoding='utf-8') as readme_file:
+    readme_file.write(readme_content)
+    readme_file.write('### Posts\n\n')
+    
+    for i in range(min(5, len(entries))):
+        date, title, filename = entries[i]
+        link = f"- [{title}](posts/{filename.replace('.md', '.html')}) / {date}"
+        readme_file.write(link + '\n')
+    
+    readme_file.write('- [All Posts](./toc.md) / [RSS](https://yuanj.top/rss.xml)\n')
+
+print("README.md has been updated with the Posts.")
