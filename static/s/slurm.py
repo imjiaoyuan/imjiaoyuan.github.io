9de793fa1c2adcdfa1975969b29bf277720e73e9
imjiaoyuan <imjiaoyuan@gmail.com>
2025-06-19 13:45:52 +0800

    update

--- static/s/slurm.py
+++ static/s/slurm.py
@@ -2,16 +2,15 @@
 import os, glob, sys
 from subprocess import call
 
-INPUT_DIR = "input"
-FILE_PATTERN = "*.fq.gz"
-OUTPUT_DIR = "output"
-COMMAND_TEMPLATE = "${INPUT_DIR}/${FILEID}.fq.gz --outdir ${OUTPUT_DIR} --threads ${SLURM_CPUS} --mem ${SLURM_MEM}G"
+INPUT_DIR = "workdir"
+FILE_PATTERN = "*.fastq.gz"
+OUTPUT_DIR = "workdir/fastqgc"
+COMMAND_TEMPLATE = "fastqc ${INPUT_DIR}/${FILEID}.fastq.gz -o ${OUTPUT_DIR}"
 
 JOB_DIR = "./jobs"
 LOG_DIR = "./logs"
-SLURM_PARTITION = "compute1,compute2"
-SLURM_CPUS = "10"
-SLURM_MEM = "100"
+SLURM_CPUS = "2"
+SLURM_MEM = "8"
 CONDA_ENV = "omics"
 
 def gen_commands():
@@ -33,12 +32,11 @@ def build_jobs():
         log_prefix = os.path.join(LOG_DIR, f"{fileid}")
         with open(job_file, "w") as f:
             f.write("#!/bin/sh\n")
-            f.write(f"#SBATCH -p {SLURM_PARTITION}\n")
             f.write(f"#SBATCH -n 1 -c {SLURM_CPUS} --mem={SLURM_MEM}g\n")
             f.write(f"#SBATCH -o {log_prefix}.out\n")
             f.write(f"#SBATCH -e {log_prefix}.err\n")
             f.write(f"#SBATCH -D {os.getcwd()}\n\n")
-            f.write("source ~/miniconda3/etc/profile.d/conda.sh\n")
+            f.write("source ~/miniforge3/etc/profile.d/conda.sh\n")
             f.write(f"conda activate {CONDA_ENV}\n\n")
             cmd = COMMAND_TEMPLATE.replace("${INPUT_DIR}", INPUT_DIR)
             cmd = cmd.replace("${FILEID}", fileid)
@@ -46,6 +44,7 @@ def build_jobs():
             f.write(cmd + "\n")
 
 def submit_jobs():
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
     for job_file in glob.glob(os.path.join(JOB_DIR, "*.sl")):
         call(["sbatch", job_file])
 
