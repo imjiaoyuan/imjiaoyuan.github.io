e0eaa6855ffc1d9db777d5b29473eb6fedefa2d8
imjiaoyuan <imjiaoyuan@gmail.com>
2025-06-22 14:38:33 +0800

    update slurm.py

--- static/s/slurm.py
+++ static/s/slurm.py
@@ -16,7 +16,7 @@ CONDA_ENV = "atac"
 def gen_commands():
     with open("commands.txt", "w") as f:
         for f_path in glob.glob(os.path.join(INPUT_DIR, f"*{FILE_SUFFIX}")):
-            fileid = os.path.basename(f_path).removesuffix(FILE_SUFFIX)
+            fileid = os.path.basename(f_path).replace(FILE_SUFFIX, "")
             cmd = COMMAND_TEMPLATE.replace("${INPUT_DIR}", INPUT_DIR) \
                                 .replace("${FILEID}", fileid) \
                                 .replace("${OUTPUT_DIR}", OUTPUT_DIR)
@@ -26,13 +26,12 @@ def build_jobs():
     os.makedirs(JOB_DIR, exist_ok=True)
     os.makedirs(LOG_DIR, exist_ok=True)
     for f_path in glob.glob(os.path.join(INPUT_DIR, f"*{FILE_SUFFIX}")):
-        fileid = os.path.basename(f_path).removesuffix(FILE_SUFFIX)
+        fileid = os.path.basename(f_path).replace(FILE_SUFFIX, "")
         job_file = os.path.join(JOB_DIR, f"{fileid}.sl")
         log_prefix = os.path.join(LOG_DIR, f"{fileid}")
         with open(job_file, "w") as f:
             f.write("#!/bin/sh\n")
             f.write(f"#SBATCH -n 1 -c {SLURM_CPUS} --mem={SLURM_MEM}g\n")
-            f.write(f"#SBATCH -o {log_prefix}.out\n")
             f.write(f"#SBATCH -e {log_prefix}.err\n")
             f.write(f"#SBATCH -D {os.getcwd()}\n\n")
             f.write("source ~/miniforge3/etc/profile.d/conda.sh\n")
