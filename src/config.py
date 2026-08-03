f088f64c9b1a7ce3a794fdaa0b85d5b99d494c24
jiaoyuan <imjiaoyuan@gmail.com>
2026-08-03 20:37:54 +0800

    add Projects page, expose feed_months in config

--- src/config.py
+++ src/config.py
@@ -8,8 +8,10 @@ SITE = {
     "static_dir": "src/assets",
     "public_dir": "public",
     "home_page": "index.md",
+    "feed_months": 12,
     "menu": [
         {"name": "Blog", "url": "/blog/"},
+        {"name": "Projects", "url": "/projects/"},
         {"name": "RSS", "url": "/atom.xml"},
     ],
     "server": {"host": "127.0.0.1", "port": 1313},
